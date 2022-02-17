e12c481df8103e566709f7c8c5a2d53dc59dfc09
baalperez <baal_perez@wp.pl>
2022-02-17 13:33:04 +0100

    30.3 - testUpdateBestsellers

---
+++ kodilla-jdbc-bazy-danych/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -0,0 +1,63 @@
+package com.kodilla.jdbc;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StoredProcTestSuite {
+
+    @Test
+    public void testUpdateVipLevels() throws SQLException {
+        // Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE READERS SET VIP_LEVEL = \"Not set\"";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM READERS WHERE VIP_LEVEL = \"Not set\"";
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+
+        // When
+        String sqlProcedureCall = "CALL UpdateVipLevels()";
+        statement.execute(sqlProcedureCall);
+
+        ResultSet rs2 = statement.executeQuery(sqlCheckTable);
+        int howMany = -1;
+        if (rs2.next()) {
+            howMany = rs2.getInt("HOW_MANY");
+        }
+
+        // Then
+        assertEquals(0, howMany);
+        rs.close();
+        statement.close();
+    }
+
+    @Test
+    void testUpdateBestsellers() throws SQLException {
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE BOOKS SET BESTSELLER = 0";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM BOOKS WHERE BESTSELLER = 1";
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+
+        // When
+        String sqlProcedureCall = "CALL UpdateBestsellers()";
+        statement.execute(sqlProcedureCall);
+
+        ResultSet rs2 = statement.executeQuery(sqlCheckTable);
+        int howMany = -1;
+        if (rs2.next()) {
+            howMany = rs2.getInt("HOW_MANY");
+        }
+
+        // Then
+        assertEquals(1, howMany);
+        rs.close();
+        statement.close();
+    }
+}
